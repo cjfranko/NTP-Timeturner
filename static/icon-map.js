0baf7588daa42ad381a88fd3785d6ad41af79e9f
Chris Frankland-Wright <christopherjfrankland@googlemail.com>
2025-08-08 00:04:47 +0100

    fix: Correct typo in icon image paths

--- static/icon-map.js
+++ static/icon-map.js
@@ -1,25 +1,25 @@
 // In this file, you can define the paths to your local icon image files.
 const iconMap = {
     ltcStatus: {
-        'LOCK': 'assets/timetuner_ltc_green.png',
-        'FREE': 'assets/timetuner_ltc_orange.png',
-        'default': 'assets/timetuner_ltc_red.png'
+        'LOCK': 'assets/timeturner_ltc_green.png',
+        'FREE': 'assets/timeturner_ltc_orange.png',
+        'default': 'assets/timeturner_ltc_red.png'
     },
     ntpActive: {
-        true: 'assets/timetuner_ntp_green.png',
-        false: 'assets/timetuner_ntp_red.png'
+        true: 'assets/timeturner_ntp_green.png',
+        false: 'assets/timeturner_ntp_red.png'
     },
     syncStatus: {
-        'IN SYNC': 'assets/timetuner_sync_green.png',
-        'CLOCK AHEAD': 'assets/timetuner_sync_orange.png',
-        'CLOCK BEHIND': 'assets/timetuner_sync_orange.png',
-        'TIMETURNING': 'assets/timetuner_timeturning.png',
-        'default': 'assets/timetuner_sync_red.png'
+        'IN SYNC': 'assets/timeturner_sync_green.png',
+        'CLOCK AHEAD': 'assets/timeturner_sync_orange.png',
+        'CLOCK BEHIND': 'assets/timeturner_sync_orange.png',
+        'TIMETURNING': 'assets/timeturner_timeturning.png',
+        'default': 'assets/timeturner_sync_red.png'
     },
     jitterStatus: {
-        'GOOD': 'assets/timetuner_jitter_green.png',
-        'AVERAGE': 'assets/timetuner_jitter_orange.png',
-        'BAD': 'assets/timetuner_jitter_red.png',
-        'default': 'assets/timetuner_jitter_red.png'
+        'GOOD': 'assets/timeturner_jitter_green.png',
+        'AVERAGE': 'assets/timeturner_jitter_orange.png',
+        'BAD': 'assets/timeturner_jitter_red.png',
+        'default': 'assets/timeturner_jitter_red.png'
     }
 };
