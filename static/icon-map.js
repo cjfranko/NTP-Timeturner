cd922d54036b6521c584a8609ac3484844e55ac6
Chris Frankland-Wright <christopherjfrankland@googlemail.com>
2025-08-07 23:22:04 +0100

    style: Replace generic icons with TimeTurner themed images

--- static/icon-map.js
+++ static/icon-map.js
@@ -1,25 +1,25 @@
 // In this file, you can define the paths to your local icon image files.
 const iconMap = {
     ltcStatus: {
-        'LOCK': 'assets/lock.svg',
-        'FREE': 'assets/lock-open.svg',
-        'default': 'assets/question-circle.svg'
+        'LOCK': 'assets/timeturner_ltc_green.png',
+        'FREE': 'assets/timeturner_ltc_orange.png',
+        'default': 'assets/timeturner_ltc_red.png'
     },
     ntpActive: {
-        true: 'assets/check-circle.svg',
-        false: 'assets/times-circle.svg'
+        true: 'assets/timeturner_ntp_green.png',
+        false: 'assets/timeturner_ntp_red.png'
     },
     syncStatus: {
-        'IN SYNC': 'assets/arrows-rotate.svg',
-        'CLOCK AHEAD': 'assets/forward.svg',
-        'CLOCK BEHIND': 'assets/backward.svg',
-        'TIMETURNING': 'assets/wand-magic-sparkles.svg',
-        'default': 'assets/question-circle.svg'
+        'IN SYNC': 'assets/timeturner_sync_green.png',
+        'CLOCK AHEAD': 'assets/timeturner_sync_orange.png',
+        'CLOCK BEHIND': 'assets/timeturner_sync_orange.png',
+        'TIMETURNING': 'assets/timeturner_timeturning.png',
+        'default': 'assets/timeturner_sync_red.png'
     },
     jitterStatus: {
-        'GOOD': 'assets/thumbs-up.svg',
-        'AVERAGE': 'assets/face-meh.svg',
-        'BAD': 'assets/thumbs-down.svg',
-        'default': 'assets/question-circle.svg'
+        'GOOD': 'assets/timeturner_jitter_green.png',
+        'AVERAGE': 'assets/timeturner_jitter_orange.png',
+        'BAD': 'assets/timeturner_jitter_red.png',
+        'default': 'assets/timeturner_jitter_red.png'
     }
 };
